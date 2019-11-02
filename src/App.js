1b413a85117ad6f66be0974cae78876c06139404
Jaycee Cheong <jaycee.cheong04@gmail.com>
2019-11-02 11:15:14 +0000

    Revert file change on App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import logo from './logo.svg';
 
 import './App.css';
 import MaterialIcon from 'material-icons-react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   //TODO componentize header
@@ -20,7 +19,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header sticky" id="header">
+        <ul>
+          <li>
+            <a>Period Friendly Bristol</a>
+          </li>
+          {/* <li>
+            <img src={logo} className="App-logo mg-left-2" alt="logo" />
+          </li> */}
+        </ul>
       </header>
+      <div className="container-middle">
+        <div className="text-wrapper">
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </p>
+        </div>
+      </div>
+      <div className="container-middle">
+        <div className="search-box-wrapper">
+          <SearchComponent />
+        </div>
+        <div className="map-container">
+        Map box to be popped into here
+        </div>
+      </div>
       <footer className="footer">
         <div className="div-ul">
           <div>
@@ -49,7 +71,6 @@ function App() {
           </div>
         </div>
       </footer>
-        <div className="map-container">Map box to be popped into here</div>
     </div>
 
   );
