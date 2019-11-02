6fb9f02f643316ed1a3fa87e61601feac3d790f3
Jaycee Cheong <jaycee.cheong04@gmail.com>
2019-11-02 13:19:58 +0000

    Update routing pattern

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter,Route } from 'react-router-dom';
+import Landing from './components/landing-page/landing';
 import Results from './containers/results';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
-    <Route path="/venues" component={Results} />	
+    <Route path="/" component={Landing} />
+    <Route path="/venues" component={Results} />
   </BrowserRouter>
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
