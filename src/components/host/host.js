427fdcb3571a663325ee920ad731645d8a7e5d0e
Jaycee Cheong <jaycee.cheong04@gmail.com>
2019-11-10 12:57:35 +0000

    Create basic submission form

--- src/components/host/host.js
+++ src/components/host/host.js
@@ -27,16 +27,81 @@ function Host() {
             </ul>
           </div>
         </div>
-        <div className="bottom-section row">
+        <div className="middle-section row">
           <div className="person padding-left-1 column">
             <Person />
           </div>
           <div className="call-to-action column">
             <p>
-              This information will be reviewed and displayed publicly. Do not enter private/personal information.            </p>
-            <p>
+              This information will be reviewed and displayed publicly. Do not enter private/personal information.
             </p>
           </div>
+        </div>
+        <div className="bottom-section row">
+          <form className="submit-host">
+            <label>
+              Email
+              <br/>
+              <input type="email" name="email" />
+            </label>
+            <label>
+              <br/>
+              Phone number
+              <br/>
+              <input type="text" name="phone"/>
+            </label>
+            <label>
+              <br/>
+              Location name
+              <br/>
+              <input type="text" name="location_name"/>
+            </label>
+            <label>
+              <br/>
+              Address
+              <br/>
+              <input type="text" name="address"/>
+            </label>
+            <label>
+              <br/>
+              Postcode
+              <br/>
+              <input type="text" name="postcode"/>
+            </label>
+            <label>
+              <br/>
+              Where will the box be located?
+              <br/>
+              <textarea rows="3" cols="33" name="box_location"/>
+            </label>
+            <label>
+              <br/>
+              Wheelchair access on site?
+              <br/>
+              <input type="radio" name="wheelchair" value="yes" />Yes
+              <input type="radio" name="wheelchair" value="no" />No
+            </label>
+            <label>
+              <br/>
+              Accessible toilet available on site?
+              <br/>
+              <input type="radio" name="toilet" value="yes" />Yes
+              <input type="radio" name="toilet" value="no" />No
+            </label>
+            <label>
+              <br/>
+              Opening hours
+              <br/>
+              <textarea name="box_location"/>
+            </label>
+            <label>
+              <br/>
+              Lorem ipsum
+              <input type="checkbox" name="terms"/>
+            </label>
+              <br/>
+              <input type="submit" value="I WANT TO BECOME A HOST"/>
+          </form>
         </div>
       </body>
     </div>
