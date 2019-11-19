cd6cbf2ce24cde00a13765b77e4844f4d41f7f36
Charlie <charlie.gillions@bristolisopen.com>
2019-11-19 17:11:52 +0000

    Added production Dockerfile. Altered .gitignore to allow build directory to be added.

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,5 @@
-# base image
-FROM node:12.2.0-alpine
+# Base NGINX image.
+FROM nginx
 
-# set working directory
-WORKDIR /app
-
-# add `/client/node_modules/.bin` to $PATH
-ENV PATH /app/node_modules/.bin:$PATH
-
-# install and cache client dependencies
-COPY package.json /app/package.json
-RUN npm install --silent
-RUN npm install react-scripts@3.0.1 -g --silent
-
-# copy the app into the container
-COPY src src
-COPY public public
-
-# start client
-CMD ["npm", "start"]
+# Copy the project files.
+COPY build /usr/share/nginx/html
---
+++ build/precache-manifest.8e85a4e0d134da59b4b58655361cefa4.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53fe95f88beb1d3e035755a04935f43b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cacc63f9aa255d392400",
+    "url": "/static/css/2.80701757.chunk.css"
+  },
+  {
+    "revision": "076c1e835eb919da4c76",
+    "url": "/static/css/main.f6f92b9f.chunk.css"
+  },
+  {
+    "revision": "cacc63f9aa255d392400",
+    "url": "/static/js/2.4e76df52.chunk.js"
+  },
+  {
+    "revision": "076c1e835eb919da4c76",
+    "url": "/static/js/main.65f505d0.chunk.js"
+  },
+  {
+    "revision": "5178fb100f9f6a9821ca",
+    "url": "/static/js/runtime-main.2877809b.js"
+  },
+  {
+    "revision": "7aa932863aea9fb88a96f357a8de3c61",
+    "url": "/static/media/about.7aa93286.svg"
+  },
+  {
+    "revision": "d13ace4828209bbdcceaeaae086d749a",
+    "url": "/static/media/circle_grey.d13ace48.svg"
+  },
+  {
+    "revision": "88581d42be28d14b70ee7957783dac6f",
+    "url": "/static/media/ellipse.88581d42.svg"
+  },
+  {
+    "revision": "9ed90b5cc27cf8d220ecceb5a21ef83b",
+    "url": "/static/media/humaaan_3.9ed90b5c.svg"
+  },
+  {
+    "revision": "0f4fa9755f480e75463e74b3dce5a3ee",
+    "url": "/static/media/lato-black-italic.0f4fa975.woff2"
+  },
+  {
+    "revision": "798eafdd87dc8f3174f76164f0685e02",
+    "url": "/static/media/lato-black-italic.798eafdd.woff"
+  },
+  {
+    "revision": "33d5f0d956f3fc30bc51f81047a2c47d",
+    "url": "/static/media/lato-black.33d5f0d9.woff2"
+  },
+  {
+    "revision": "f80bda6afd19534368443a3d0323a140",
+    "url": "/static/media/lato-black.f80bda6a.woff"
+  },
+  {
+    "revision": "0b6bb6725576b072c5d0b02ecdd1900d",
+    "url": "/static/media/lato-bold-italic.0b6bb672.woff2"
+  },
+  {
+    "revision": "9c7e4e9eb485b4a121c760e61bc3707c",
+    "url": "/static/media/lato-bold-italic.9c7e4e9e.woff"
+  },
+  {
+    "revision": "cccb897485813c7c256901dbca54ecf2",
+    "url": "/static/media/lato-bold.cccb8974.woff2"
+  },
+  {
+    "revision": "d878b6c29b10beca227e9eef4246111b",
+    "url": "/static/media/lato-bold.d878b6c2.woff"
+  },
+  {
+    "revision": "b4da0df63131b83ddeec1febb5b15374",
+    "url": "/static/media/lato-hairline-italic.b4da0df6.woff"
+  },
+  {
+    "revision": "d9cf517802956cd88eadcb9158aa6dec",
+    "url": "/static/media/lato-hairline-italic.d9cf5178.woff2"
+  },
+  {
+    "revision": "482fe0a9e92d9c5ff7fec117ca54c8ae",
+    "url": "/static/media/lato-hairline.482fe0a9.woff"
+  },
+  {
+    "revision": "ada4458b361d5e72bcbd19da105afdc5",
+    "url": "/static/media/lato-hairline.ada4458b.woff2"
+  },
+  {
+    "revision": "38d2399f6c10d8ba1d8d45ba0c440ad5",
+    "url": "/static/media/lato-heavy-italic.38d2399f.woff"
+  },
+  {
+    "revision": "46f0461b6e19880fe446f094bbe787f4",
+    "url": "/static/media/lato-heavy-italic.46f0461b.woff2"
+  },
+  {
+    "revision": "2ce4d82354fdf1be1788c526d94eefc1",
+    "url": "/static/media/lato-heavy.2ce4d823.woff"
+  },
+  {
+    "revision": "c9cbbdc3762c340d5d37073a54971487",
+    "url": "/static/media/lato-heavy.c9cbbdc3.woff2"
+  },
+  {
+    "revision": "314210a4825a7cc8ca7db893dfd9d283",
+    "url": "/static/media/lato-light-italic.314210a4.woff2"
+  },
+  {
+    "revision": "b55e385f24f0f9f724dac935fe292ecf",
+    "url": "/static/media/lato-light-italic.b55e385f.woff"
+  },
+  {
+    "revision": "7244318390cc4d36aac4a613ff42d308",
+    "url": "/static/media/lato-light.72443183.woff2"
+  },
+  {
+    "revision": "90301aa07d780a09812229d6375c3b28",
+    "url": "/static/media/lato-light.90301aa0.woff"
+  },
+  {
+    "revision": "4d295621045bd774d4beed9d6d5796e4",
+    "url": "/static/media/lato-medium-italic.4d295621.woff2"
+  },
+  {
+    "revision": "aa1a75124661b40a6770dc0d8274ddaa",
+    "url": "/static/media/lato-medium-italic.aa1a7512.woff"
+  },
+  {
+    "revision": "0996d39c4cf5d223a14559dfa37047fd",
+    "url": "/static/media/lato-medium.0996d39c.woff2"
+  },
+  {
+    "revision": "acbd6ecc97c80340e9bf00ea80063234",
+    "url": "/static/media/lato-medium.acbd6ecc.woff"
+  },
+  {
+    "revision": "4eb103b4d12be57cb1d040ed5e162e9d",
+    "url": "/static/media/lato-normal-italic.4eb103b4.woff2"
+  },
+  {
+    "revision": "f28f2d6482446544ef1ea1ccc6dd5892",
+    "url": "/static/media/lato-normal-italic.f28f2d64.woff"
+  },
+  {
+    "revision": "27bd77b9162d388cb8d4c4217c7c5e2a",
+    "url": "/static/media/lato-normal.27bd77b9.woff"
+  },
+  {
+    "revision": "bd03a2cc277bbbc338d464e679fe9942",
+    "url": "/static/media/lato-normal.bd03a2cc.woff2"
+  },
+  {
+    "revision": "80f6811f6c30735dab68a01372d8b78f",
+    "url": "/static/media/lato-semibold-italic.80f6811f.woff2"
+  },
+  {
+    "revision": "9fcec04cdba9253e002e740a7cd743fe",
+    "url": "/static/media/lato-semibold-italic.9fcec04c.woff"
+  },
+  {
+    "revision": "8b4f872c5de19974857328d06d3fe48f",
+    "url": "/static/media/lato-semibold.8b4f872c.woff2"
+  },
+  {
+    "revision": "c2b50f4a7d908c8d06f5b05ec135e166",
+    "url": "/static/media/lato-semibold.c2b50f4a.woff"
+  },
+  {
+    "revision": "16d14ad314296a4644d550c8f20bd407",
+    "url": "/static/media/lato-thin-italic.16d14ad3.woff"
+  },
+  {
+    "revision": "880cffe4febaa7476b6aee71db68b7e4",
+    "url": "/static/media/lato-thin-italic.880cffe4.woff2"
+  },
+  {
+    "revision": "0faa1074c17a74a7f5e32cbe6f9d76f3",
+    "url": "/static/media/lato-thin.0faa1074.woff2"
+  },
+  {
+    "revision": "13fa4c60e0ee7ea5fe2bd84059fb8cac",
+    "url": "/static/media/lato-thin.13fa4c60.woff"
+  },
+  {
+    "revision": "1ff8c29e430d5dad313bbed7064e25ad",
+    "url": "/static/media/pin-1.1ff8c29e.svg"
+  },
+  {
+    "revision": "26dc69aeb730f2facb2cf055326e19e2",
+    "url": "/static/media/pin-2.26dc69ae.svg"
+  },
+  {
+    "revision": "3dba0a31606dc13ac443e756da8b4289",
+    "url": "/static/media/pin-3.3dba0a31.svg"
+  },
+  {
+    "revision": "a3173621e513e8dc7016f4ef44ed7026",
+    "url": "/static/media/pin-4.a3173621.svg"
+  },
+  {
+    "revision": "619c84a7dd50ff9d84199d07e68fbaea",
+    "url": "/static/media/pin-5.619c84a7.svg"
+  },
+  {
+    "revision": "885539ecbbadac5cc5547f152ece8f4e",
+    "url": "/static/media/pin-6.885539ec.svg"
+  },
+  {
+    "revision": "ad3db3c57b2faf4f486519a5a5c6544f",
+    "url": "/static/media/pin-7.ad3db3c5.svg"
+  },
+  {
+    "revision": "8f38d2ebd2b07bcdd3434c846168d43c",
+    "url": "/static/media/pin-8.8f38d2eb.svg"
+  },
+  {
+    "revision": "64793d9ac3d96820251abdff31ffc835",
+    "url": "/static/media/pin-9.64793d9a.svg"
+  }
+]);
