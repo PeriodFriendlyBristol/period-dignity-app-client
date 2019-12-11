13f9f71ec6c888662bab7aa90325d35f40a48c98
Jonah <jonah.freeland@mrbandfriends.co.uk>
2019-12-11 18:23:27 +0000

    update header to new designs

--- src/App.js
+++ src/App.js
@@ -5,21 +5,6 @@ import Footer from "./components/footer/footer";
 import "./App.css";
 
 function App() {
-  const scrollFunction = () => {
-    if (
-      document.body.scrollTop > 50 ||
-      document.documentElement.scrollTop > 50
-    ) {
-      document.getElementById("header").style.height = "60px";
-    } else {
-      document.getElementById("header").style.height = "80px";
-    }
-  };
-
-  window.onscroll = () => {
-    scrollFunction();
-  };
-
   return (
     <div className="App">
       <Header></Header>
