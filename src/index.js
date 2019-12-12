d2e9da6af415ed52e03b262367b40577c47b0d11
herecomesjaycee <jaycee.cheong04@gmail.com>
2019-12-12 09:02:51 +0000

    Update the order of common elements

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer"
 import About from "./components/about/about";
 import Landing from "./components/landing-page/landing";
 import Venues from "./containers/venues";
@@ -12,11 +14,13 @@ import Host from './components/host/host';
 
 ReactDOM.render(
   <BrowserRouter>
+    <Header/>
     <App />
     <Route exact path="/" component={Landing} />
     <Route path="/venues" component={Venues} />
     <Route path="/about" component={About} />
     <Route path="/host" component={Host} />
+    <Footer />
   </BrowserRouter>,
   document.getElementById("root")
 );
