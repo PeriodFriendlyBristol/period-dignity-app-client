4fd1fdc15dc25368d3ddf93bfde8da6232de7e66
herecomesjaycee <jaycee.cheong04@gmail.com>
2019-12-12 16:41:48 +0000

    Added icons

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -7,9 +7,24 @@ class Nav extends React.Component {
     return (
       <nav className="navbar">
          <ul>
-          <li class="nav-item"><a href="/">Find a Box</a></li>
-          <li class="nav-item"><a href="/host">Host a Box</a></li>
-          <li class="nav-item"><a href="/about">About</a></li>
+          <li class="nav-item">
+            <a href="/">
+              <MaterialIcon icon="search" color="white"/>
+              <div className="nav-text">Find a Box</div>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="/host">
+              <MaterialIcon icon='favorite' color="white"/>
+              <div className="nav-text">Host a Box</div>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="/about">
+              <MaterialIcon icon='person' color="white"/>
+              <div className="nav-text">About</div>
+            </a>
+          </li>
         </ul>
       </nav>
     )
