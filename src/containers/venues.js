0f94fd8637a09cdae21d5e13b2b9ea8d7955beba
Jonah <jonah.freeland@mrbandfriends.co.uk>
2019-12-14 09:54:53 +0000

    fix off by one error

--- src/containers/venues.js
+++ src/containers/venues.js
@@ -3,10 +3,6 @@ import MapComponent from "../components/map/map.js";
 import VenueList from "../components/venuelist/venuelist.js";
 
 export default class Venues extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div>
