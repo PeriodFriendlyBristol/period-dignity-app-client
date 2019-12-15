d2897c4d5aedf29b77658dc71236560e29f01cbb
herecomesjaycee <jaycee.cheong04@gmail.com>
2019-12-15 17:44:41 +0000

    Using NavLink instead of native link

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MaterialIcon from 'material-icons-react';
+import {NavLink} from 'react-router-dom';
 import './nav.css';
 
 class Nav extends React.Component {
@@ -7,24 +8,18 @@ class Nav extends React.Component {
     return (
       <nav className="navbar">
          <ul className="nav-list">
-          <li className="nav-item">
-            <a href="/">
-              <MaterialIcon icon="search" alt="Find a Box"/>
-              <div className="nav-text">Find a Box</div>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="/host">
-              <MaterialIcon icon='favorite' alt="Host a Box"/>
-              <div className="nav-text">Host a Box</div>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="/about">
-              <MaterialIcon icon='person' alt="About"/>
-              <div className="nav-text">About</div>
-            </a>
-          </li>
+          <NavLink to="/" activeClassName="active" className="nav-item">
+            <MaterialIcon icon="search" alt="Find a Box"/>
+            <div className="nav-text">Find a Box</div>
+          </NavLink>
+          <NavLink to="/host" activeClassName="active" className="nav-item">
+            <MaterialIcon icon="favorite" alt="Host a Box"/>
+            <div className="nav-text">Host a Box</div>
+          </NavLink>
+          <NavLink to="/about" activeClassName="active" className="nav-item">
+            <MaterialIcon icon="person" alt="About"/>
+            <div className="nav-text">About</div>
+          </NavLink>
         </ul>
       </nav>
     )
