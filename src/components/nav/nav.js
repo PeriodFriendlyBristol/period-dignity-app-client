6c91500b8e47a7e27e0c4da7ec7a4ee75bb1f7ef
herecomesjaycee <jaycee.cheong04@gmail.com>
2019-12-15 18:14:58 +0000

    Using active class in NavLink

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import MaterialIcon from 'material-icons-react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './nav.css';
 
 class Nav extends React.Component {
   render () {
     return (
       <nav className="navbar">
+        <MaterialIcon/>
          <ul className="nav-list">
-          <NavLink to="/" activeClassName="active" className="nav-item">
-            <MaterialIcon icon="search" alt="Find a Box"/>
+          <NavLink to="/" activeClassName="active" className="nav-item" id="find">
             <div className="nav-text">Find a Box</div>
           </NavLink>
-          <NavLink to="/host" activeClassName="active" className="nav-item">
-            <MaterialIcon icon="favorite" alt="Host a Box"/>
+          <NavLink to="/host" activeClassName="active" className="nav-item" id="host">
             <div className="nav-text">Host a Box</div>
           </NavLink>
-          <NavLink to="/about" activeClassName="active" className="nav-item">
-            <MaterialIcon icon="person" alt="About"/>
+          <NavLink to="/about" activeClassName="active" className="nav-item" id="about">
             <div className="nav-text">About</div>
           </NavLink>
         </ul>
