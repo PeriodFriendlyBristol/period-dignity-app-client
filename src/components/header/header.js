ba2d2449fc2fdaf767fee8840e2636eb45559036
Charlie <charlie.gillions@bristolisopen.com>
2020-09-29 14:54:38 +0100

    Added Covid message.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,11 +4,16 @@ import "./header.css";
 class Header extends React.Component {
   render() {
     return (
-      <header id="header">
-        <a href="/">
-          <div className="header">Period Friendly Bristol</div>
-        </a>
-      </header>
+      <div>
+        <header id="header">
+          <a href="/">
+            <div className="header">Period Friendly Bristol</div>
+          </a>
+        </header>
+        <div style={{ 'background-color': 'orange', 'padding': '.5em' }}>
+          Most of our Donation Points and Collections Centres are still closed due to Covid-19. Please contact your local food bank if you need to access period products. We will keep you updated as our corporate partners begin a phased return to work and our collection centres reopen. The PFB team.
+        </div>
+      </div>
     );
   }
 }
